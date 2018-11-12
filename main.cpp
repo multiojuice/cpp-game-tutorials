6a72bc094edd2bdae9d174475c14b9e65187eca5
Owen J Sullivan <multiojuice@gmail.com>
2018-11-11 18:54:10 -0800

    Added main loop and escape from window

--- main.cpp
+++ main.cpp
@@ -6,5 +6,33 @@ int main() {
     VideoMode vm(1920,1080);
 
     RenderWindow window(vm, "Timber", Style::Fullscreen);
+
+    while (window.isOpen()) {
+
+        /*
+         * ********************
+         * Handle Player Input
+         * ********************
+        */
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+        }
+
+        /*
+         * ********************
+         * Update the scene
+         * ********************
+        */
+
+        /*
+         * ********************
+         * Draw the scene
+         * ********************
+         */
+        window.clear();
+        window.display();
+
+    }
+
     return 0;
 }
