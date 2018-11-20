08f300459bce25fb397149adb50fda182aee4d77
Owen J Sullivan <multiojuice@gmail.com>
2018-11-19 23:53:54 -0800

    Added key functionalities and state changing

--- zombie.cpp
+++ zombie.cpp
@@ -30,8 +30,82 @@ int main()
     IntRect arena;
 
     while (window.isOpen()) {
+        // Poll for Events
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::KeyPressed) {
+                if (event.key.code == Keyboard::Return && state == State::PLAYING) {
+                    state = State::PAUSED;
+                } else if (event.key.code == Keyboard::Return && state == State::PAUSED) {
+                    state = State::PLAYING;
+                    clock.restart();
+                } else if (event.key.code == Keyboard::Return && state == State::GAME_OVER) {
+                    state = State::LEVELING_UP;
+                }
 
+                if (state == State::PLAYING) {
+
+                }
+            }
+        } // End of polling
+
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+        }
+
+        if (state == State::LEVELING_UP) {
+            if (Keyboard::isKeyPressed(Keyboard::Num1)) {
+                state = State::PLAYING;
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Num2)) {
+                state = State::PLAYING;
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Num3)) {
+                state = State::PLAYING;
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Num4)) {
+                state = State::PLAYING;
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Num5)) {
+                state = State::PLAYING;
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Num6)) {
+                state = State::PLAYING;
+            }
+
+            if(state == State::PLAYING) {
+                arena.width = 500;
+                arena.height = 500;
+                arena.top = 0;
+                arena.left = 0;
+
+                int tileSize = 50;
+
+                player.spawn(arena, resolution, tileSize);
+            }
+
+        }
+
+        if (state == State::PLAYING) {
+            // Handle movement
+            if (Keyboard::isKeyPressed(Keyboard::W)) {
+                player.moveUp();
+            } else player.stopUp();
+            if (Keyboard::isKeyPressed(Keyboard::S)) {
+                player.moveDown();
+            } else player.stopDown();
+            if (Keyboard::isKeyPressed(Keyboard::A)) {
+                player.moveLeft();
+            } else player.stopLeft();
+            if (Keyboard::isKeyPressed(Keyboard::D)) {
+                player.moveRight();
+            } else player.stopRight();
+        }
     }
-
     return 0;
 }
