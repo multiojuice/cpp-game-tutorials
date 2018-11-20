1e4c149bfd24b4c8c2edb5a67acdeca4578ab7aa
Owen J Sullivan <multiojuice@gmail.com>
2018-11-19 22:02:39 -0800

    Initialized variables for game loop

--- zombie.cpp
+++ zombie.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include <sstream>
+#include "Player.h"
 
 using namespace sf;
 
@@ -9,6 +8,32 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!";
+    enum class State {
+        PAUSED, PLAYING, LEVELING_UP, GAME_OVER
+    };
+
+    State state = State::GAME_OVER;
+
+    Vector2f resolution;
+    resolution.x = VideoMode::getDesktopMode().width;
+    resolution.y = VideoMode::getDesktopMode().height;
+
+    RenderWindow window(VideoMode(resolution.x, resolution.y), "Zombie Arena", Style::Fullscreen);
+
+    View mainView(FloatRect(0, 0, resolution.x, resolution.y));
+
+    Clock clock;
+    Time gameTimeTotal;
+
+    Vector2f mouseWorldPosition;
+    Vector2i mouseScreenPosition;
+
+    Player player;
+    IntRect arena;
+
+    while (window.isOpen()) {
+
+    }
+
     return 0;
 }
